94123f7cd665bad9eb2279da581516b45253d42c
BenjaminJSLee <benjaminjslee@gmail.com>
2021-01-18 03:12:31 -0800

    Add styles and onClick prop to MovementList

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,22 @@ function App() {
       id: 1,
       start: { lat: 20.0001, lng: 20.0001},
       end: { lat: 21.0001, lng: 20.0001},
-      color: "#FFFFFF",
-      description: "",
+      color: "#FF0000",
+      description: "Material delivery to Fake St, Toronto, Ontario",
     },
     { 
       id: 2,
       start: { lat: -1.2884, lng: 36.8233},
       end: { lat: 0, lng: 37.8233},
-      color: "#000000",
-      description: "",
+      color: "#00FF00",
+      description: "Material delivery to Fake St, Toronto, Ontario",
+    },
+    { 
+      id: 3,
+      start: { lat: -2.2884, lng: 36.8233},
+      end: { lat: 0, lng: 37.8233},
+      color: "#0000FF",
+      description: "Material delivery to Fake St, Toronto, Ontario",
     },
   ];
 
--- src/components/MovementList.tsx
+++ src/components/MovementList.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Movement from './Movement';
+import './MovementList.scss';
 
 
 const MovementList = (props: any) => {
+  const [selected, setSelected]: [number | null, Function] = useState(null);
 
   const movements = props.movements.map((movement: any) => {
     return (
       <Movement
-        start={movement.start}
-        end={movement.end}
-        description={movement.description}
+        key={movement.id}
+        onClick={() => setSelected(movement.id)}
+        selected={selected === movement.id}
+        movement={movement}
       />
     )
   });
