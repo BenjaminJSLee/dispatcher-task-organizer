79a27ab77e666871bd4748fde716eba28382526d
BenjaminJSLee <benjaminjslee@gmail.com>
2021-01-18 02:43:05 -0800

    Finish styles and add props
    Add "id" and "color" prop to component.
    Add inline styles for color customization.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import './App.scss';
+import Movement from './components/Movement';
 
 function App() {
   return (
     <div>
-
+      <Movement 
+      selected
+        movement={{
+          id: 1,
+          start: {lat: 2.0001, lng: 1.0011},
+          end: {lat: 20.2821, lng: 15.0913},
+          description: "Groceries for Safeway at 1234 Fake St, Toronto, Ontario",
+          color: "#54a0ff",
+        }}
+      />
+      <Movement 
+        movement={{
+          id: 2,
+          start: {lat: 2.0001, lng: 1.0011},
+          end: {lat: 20.2821, lng: 15.0913},
+          description: "Groceries for Safeway at 1234 Fake St, Toronto, Ontario",
+          color: "#00FF00",
+        }}
+      />
     </div>
   );
 }
--- src/components/Movement.tsx
+++ src/components/Movement.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
 import './Movement.scss';
 
-interface LatLng {
+interface ILatLng {
   lat: number;
   lng: number;
 }
 
-const Movement = (props: {start: LatLng, end: LatLng, description: string}) => {
+interface IMovement {
+  id: number,
+  start: ILatLng,
+  end: ILatLng,
+  description: string,
+  color: string;
 
-  const formatLatLng = (latlng: LatLng) => {
+}
+
+const Movement = (props: { selected?: boolean, movement: IMovement}) => {
+
+  const colorScheme = {
+    background: `#ffffff linear-gradient(90deg,${props.movement.color},${props.movement.color} 1em, ${props.movement.color}37 0) padding-box`,
+    border: `0.3em solid ${props.selected ? props.movement.color : `${props.movement.color}37`}`,
+  };
+
+  const formatLatLng = (latlng: ILatLng) => {
     return `Latitude: ${latlng.lat}; Longitude: ${latlng.lng}`;
   };
 
   return (
-    <div className="movement">
-      <header>
-        <div>Pick-up (S) at {formatLatLng(props.start)}</div>
-        <div>Drop-off (E) at {formatLatLng(props.end)}</div>
+    <div className="movement" style={colorScheme}>
+      <header style={{borderColor: props.movement.color}}>
+        <div>Pick-up (S) at {formatLatLng(props.movement.start)}</div>
+        <div>Drop-off (E) at {formatLatLng(props.movement.end)}</div>
       </header>
-      <p>{props.description}</p>
+      <div>{props.movement.description}</div>
     </div>
   );
 };
