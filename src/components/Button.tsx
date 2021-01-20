a0a94805e7584bae2c58aa0525103fefa1c05173
BenjaminJSLee <benjaminjslee@gmail.com>
2021-01-20 00:04:37 -0800

    Update button styles and naming conventions

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,9 +5,9 @@ import './Button.scss';
 const Button = (props: any) => {
   let buttonClass = classNames('button', (props.disabled && 'button--disabled')
   || {
-    'button--delete': props.delete,
-    'button--add': props.add,
-    'button--edit': props.edit,
+    'button--danger': props.danger,
+    'button--success': props.success,
+    'button--warning': props.warning,
  });
   return (
     <button type="button" disabled={props.disabled} className={buttonClass} onClick={props.onClick}>
