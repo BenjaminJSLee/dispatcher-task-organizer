7f1922606a3efaacbdb781894840e1c5eef79d9c
BenjaminJSLee <benjaminjslee@gmail.com>
2021-01-19 20:11:42 -0800

    Add buttons and styles

--- src/components/MovementController.tsx
+++ src/components/MovementController.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import MovementList from './MovementList';
+import Button from './Button';
+
+import './MovementController.scss';
 
 
 const MovementController = (props: any) => {
   return (
-    <div>
+    <div className="movement-controller">
+      { 
       <MovementList 
         movements={props.movements}
-      />
+      /> 
+      }
+      {
+        
+      }
+      <div className="button-list">
+        <Button add
+        
+        >ADD</Button>
+        <Button edit
+        
+        >EDIT</Button>
+        <Button delete
+        
+        >DELETE</Button>
+      </div>
     </div>
   );
 };
